4515531d2fc64b7fb17684abba8dc23251e46e7c
samer-habash <sam.habash1984@gmail.com>
2020-08-06 11:42:05 +0300

    Changed filter dates in Hourly-split script

--- Methods-of-Extracting-logfiles/Hourly-Split.sh
+++ Methods-of-Extracting-logfiles/Hourly-Split.sh
@@ -5,8 +5,8 @@ input_log=$1
 # Date-Log-Format = it has multiple choices and can be with spaces and dashes
 #             e.g : 20200101 00:00:00
 #                   20200101-00:00:00
-#                   20200101-00:00:00
-#                   20200101-00:00:00
+#                   2020-01-01 00:00:00
+#                   2020-01-01-00:00:00
 
 Initial_Time=$(grep -aoP -m1 "^[0-9]{4}[-]?[0-9]{2}[-]?[0-9]{2}[-\s]?[0-9]{2}:[0-9]{2}" ${input_log})
 End_Time=$(bash -c 'tail -n +0 --pid=$$ -f ${input_log} | { grep -aoP -m1 "^[0-9]{4}[-]?[0-9]{2}[-]?[0-9]{2}[-\s]?[0-9]{2}:[0-9]{2}" && kill $$ ;}' | head -1)
